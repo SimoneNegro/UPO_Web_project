0e154f18bd66fc4def56934ee1c54c7e0e651c2c
BeastOfShadow <simone.negro.2002@gmail.com>
2024-03-14 16:46:33 +0100

    Validation form error handler

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,7 +2,7 @@
 function togglePasswordVisibility(op) {
     var passwordInput;
     var passwordToggle = document.getElementById("show-password-toggle");
-    if(op == 0)
+    if (op == 0)
         passwordInput = document.getElementById("current-password");
     else
         passwordInput = document.getElementById("new-password");
@@ -15,16 +15,7 @@ function togglePasswordVisibility(op) {
     }
 }
 
-
-function validateLoginForm() {
-    var mail = document.getElementById("email");
-    alert(validateMail(mail));
-}
-
-function validateSignupForm() {
-    
-}
-
-function validateMail(mail) {
-    return mail.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) !== null; 
-}
+$("#sign-in-submit").on("submit", function (e) {
+    e.preventDefault();
+    console.log('your message');
+});
--- public/js/login_validation.js
+++ public/js/login_validation.js
@@ -1,16 +1,37 @@
-var $ = require("jquery");
-
+// verify if email and password have valid values
 $(document).ready(function () {
     $("#login-form").validate({
+        // those are rules that field must have to be validated
         rules: {
             email: {
                 required: true,
                 email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
             }
         },
+
+        // those are messages that explain why the fields are not validated
         messages: {
             email: {
                 email: 'Invalid email address'
+            },
+            password: {
+                required: 'Please provide a password',
+                minlength: 'Password must be at least 5 characters long'
+            }
+        },
+
+        // we take the span id to determine where the message goes in the code
+        errorPlacement: function (error, element) {
+            var id = element.attr('id');
+            var errorElement = $('#' + id + '-error');
+            if (errorElement.length) {
+                error.appendTo(errorElement);
+            } else {
+                error.insertAfter(element);
             }
         }
     })
