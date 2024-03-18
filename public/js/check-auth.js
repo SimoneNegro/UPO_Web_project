ad1cc2040c7017c143ae25a3682959bcca62a1d4
BeastOfShadow <simone.negro.2002@gmail.com>
2024-03-18 02:52:40 +0100

    Login and signup done

---
+++ public/js/check-auth.js
@@ -0,0 +1,6 @@
+// function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     res.redirect("/login");
+// }
--- routes/login.js
+++ routes/login.js
@@ -2,14 +2,22 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/', function (req, res, next) {
-    res.render('login');
+router.get('/', function(req, res, next) {
+    const errorMessage = req.session.errorMessage;
+    // clean error message session
+    req.session.errorMessage = null;
+    res.render('login', { message: errorMessage }); // Passa i messaggi flash alla vista
 });
 
-router.post('/', passport.authenticate('local', {
-    successRedirect: '/home',
-    failureRedirect: '/',
-    failureFlash: true // Enable flash messages for failure
-}));
+router.post('/', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) {
+            req.session.errorMessage = 'Email or password are incorrect.';
+            return res.redirect('/login');
+        }
+        return res.redirect('/home');
+    })(req, res, next);
+});
 
 module.exports = router;
