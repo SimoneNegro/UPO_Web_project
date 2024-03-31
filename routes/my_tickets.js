9332b4e6ff4077caea7feca2cbf0b5ee55a2dc45
BeastOfShadow <simone.negro.2002@gmail.com>
2024-03-31 01:02:43 +0100

    User id error handled

--- routes/my_tickets.js
+++ routes/my_tickets.js
@@ -4,17 +4,17 @@ const db = require('../db');
 
 // my-tickets root
 router.get('/', function (req, res, next) {
-    if (!req.user) { res.render('my_tickets', { title: "My Tickets", user: req.user }); }
+    if (!req.user) { return res.render('my_tickets', { title: "My Tickets", user: req.user }); }
 
     // get how many tickets the user opened: if he opened more than one ticket, he see all tickets, otherwise he see another message
     db.get('SELECT COUNT(*) AS num_user_ticket FROM ticket t INNER JOIN utente u ON t.id_utente = u.id WHERE u.id = ?', [req.user.id], function (err, row) {
         if (err) { return done(err); }
 
-        if (row.num_user_ticket == 0) { res.render('my_tickets', { title: "My Tickets", user: req.user, num_tickets: row }); }
+        if (row.num_user_ticket == 0) { return res.render('my_tickets', { title: "My Tickets", user: req.user, num_tickets: row }); }
 
         // get all user tickets
         db.all('SELECT * FROM ticket WHERE id_utente = ?', [req.user.id], function (err, rows) {
-            res.render('my_tickets', { title: "My Tickets", user: req.user, num_tickets: row, tickets: rows });
+            return res.render('my_tickets', { title: "My Tickets", user: req.user, num_tickets: row, tickets: rows });
         });
     });
 });
