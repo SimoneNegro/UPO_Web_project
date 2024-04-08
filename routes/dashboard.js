569f11c1dd338f7cd200148a603d728afaaf1856
BeastOfShadow <simone.negro.2002@gmail.com>
2024-04-08 22:11:58 +0200

    Dashboard setup

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+
+const DataBase = require("../db"); // db.js
+const db = new DataBase();
+
 const { isStaff } = require('../public/js/auth');
 
-router.get('/', function (req, res, next) {
-    if (isStaff(req)) { return res.render('admin/dashboard', { title: 'Ticket Dashboard', user: req.user }); }
-    return res.redirect('/');
+router.get('/', async function (req, res, next) {
+    if (!isStaff(req)) { return res.redirect('/'); }
+
+    try {
+        const num_tickets = await db.numberOfManagedTicket(req.user.id);
+        return res.render('admin/dashboard', { title: 'Ticket Dashboard', user: req.user, num_tickets: num_tickets });
+    } catch (err) {
+        console.error("Error:", err);
+        // TODO: handle error
+    }
 });
 
 module.exports = router;
