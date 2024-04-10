94764e6bcfe3dbcdd850d8fef8a66e4346da0da9
BeastOfShadow <simone.negro.2002@gmail.com>
2024-04-10 14:30:17 +0200

    tickets visualization, visualize data

--- routes/closed_tickets.js
+++ routes/closed_tickets.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { isStaff } = require('../public/js/auth');
+const {isStaff} = require('../public/js/auth');
 
-router.get('/', function (req, res, next) {
-    if (isStaff(req)) { return res.render('admin/closed_tickets', { title: "Closed Tickets", user: req.user }); }
-    return res.redirect('/');
+const DataBase = require("../db"); // db.js
+const db = new DataBase();
+
+
+router.get('/', async function (req, res, next) {
+    if (!isStaff(req)) {
+        return res.redirect('/');
+    }
+
+    try {
+        const num_closed_tickets = await db.numberOfClosedTickets(req.user.id);
+        const resolved_tickets = await db.resolvedTickets(req.user.id);
+        const closed_tickets = await db.closedTickets(req.user.id);
+        const cancelled_tickets = await db.cancelledTickets(req.user.id);
+
+        return res.render('admin/closed_tickets', {
+            title: "Closed Tickets",
+            user: req.user,
+            num_closed_tickets: num_closed_tickets,
+            resolved_tickets: resolved_tickets,
+            closed_tickets: closed_tickets,
+            cancelled_tickets: cancelled_tickets
+        });
+    } catch (err) {
+        console.error("Error:", err);
+        // TODO: handle error
+    }
 });
 
 module.exports = router;
--- routes/view_tickets.js
+++ routes/view_tickets.js
@@ -5,15 +5,22 @@ const DataBase = require("../db"); // db.js
 const db = new DataBase();
 
 // const { userId } = require('../public/js/jwt-token');
-const { isStaff } = require('../public/js/auth');
+const {isStaff} = require('../public/js/auth');
 
 router.get('/', async function (req, res, next) {
-    if (!isStaff(req)) { return res.redirect('/'); }
+    if (!isStaff(req)) {
+        return res.redirect('/');
+    }
 
     try {
         const tickets = await db.allOpenTickets();
         const num_tickets = await db.numberOfManagedTicket(req.user.id);
-        return res.render('admin/view_tickets', { title: "View Tickets", user: req.user, tickets: tickets, num_tickets: num_tickets });
+        return res.render('admin/view_tickets', {
+            title: "View Tickets",
+            user: req.user,
+            tickets: tickets,
+            num_tickets: num_tickets
+        });
     } catch (err) {
         console.error("Error:", err);
         // TODO: handle error
@@ -21,7 +28,9 @@ router.get('/', async function (req, res, next) {
 });
 
 router.post('/', async function (req, res, next) {
-    if (!isStaff(req)) { return res.redirect('/'); }
+    if (!isStaff(req)) {
+        return res.redirect('/');
+    }
     try {
         let data = Math.floor(new Date().getTime() / 1000.0);
         await db.addManageTicket(req.user.id, req.body.ticket_id, data);
