b86ec82888875097fc1446c317fda50b3cccf4d0
BeastOfShadow <simone.negro.2002@gmail.com>
2024-04-14 23:26:23 +0200

    grahics improvement

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { isStaff } = require('../public/js/auth');
 
 // home root
 // add isStaff and isAdmin
 router.get('/', function (req, res, next) {
-    if (isStaff(req)) { return res.redirect('/admin'); }
     return res.render('home', { title: "NGR", user: req.user });
 });
 
--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,8 @@ const passport = require('passport');
 const DataBase = require("../db"); // db.js
 const db = new DataBase();
 
-const { generateToken } = require('../public/js/jwt-token');
+const {generateToken} = require('../public/js/jwt-token');
+const {isStaff} = require('../public/js/auth');
 
 let returnUrl = "";
 
@@ -15,23 +16,32 @@ router.get('/', function (req, res, next) {
     req.session.errorMessage = null;
     returnUrl = req.query.return;
 
-    return res.render('login', { message: errorMessage });
+    return res.render('login', {message: errorMessage});
 });
 
 // check if user email and password are correct
 router.post('/', function (req, res, next) {
     passport.authenticate('local', function (err, user, info) {
-        if (err) { return next(err); }
+        if (err) {
+            return next(err);
+        }
         if (!user) {
             req.session.errorMessage = 'Email or password are incorrect.';
             return res.redirect('/login');
         }
         req.login(user, async function (err) {
             try {
-                if (err) { return next(err); }
+                if (err) {
+                    return next(err);
+                }
                 await db.addTokenToUser(user.id, generateToken(user.id));
-                if (!returnUrl) { return res.redirect('/'); }
-                return res.redirect(returnUrl);
+                if (isStaff(req)) {
+                    return res.redirect('/admin');
+                }
+                if (returnUrl) {
+                    return res.redirect(returnUrl);
+                }
+                return res.redirect('/');
             } catch (err) {
                 console.error("Error:", err);
                 // TODO: handle error
