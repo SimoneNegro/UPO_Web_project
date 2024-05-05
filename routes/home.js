0a38e07bbb73ef2296dc1cd88c6930c852a4ab3e
BeastOfShadow <simone.negro.2002@gmail.com>
2024-05-05 16:53:58 +0200

    Solved routing

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const {isStaff} = require('../public/js/auth');
+
 // home root
 // add isStaff and isAdmin
 router.get('/', function (req, res, next) {
+    if(isStaff(req)) 
+        return res.redirect('/admin');
+    
     return res.render('home', {title: "NGR", user: req.user});
 });
 
